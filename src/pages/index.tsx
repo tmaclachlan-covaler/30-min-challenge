cbe5a38f0ea41734c130fd8e6e293fb6828fa379
Tyler Maclachlan <tyler@covaler.com>
2023-05-17 14:49:02 +0200

    Filter out membership product if you already have it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 import { Box, Stack, Typography } from "@mui/material";
 import ProductCard from "@/components/ProductCard";
 import useProductsQuery from "@/hooks/queries/useProductsQuery";
+import useMembership from "@/hooks/useMembership";
 
 export default function Home() {
   const { data: products = [], isLoading: productsLoading } =
     useProductsQuery();
+
+  const [hasMembership] = useMembership();
 
   if (productsLoading) {
     return <Box>Loading...</Box>;
@@ -16,9 +19,15 @@ export default function Home() {
   return (
     <Box justifyContent="center" flex={1} pt={4}>
       <Stack direction="row" spacing={3} justifyContent="center">
-        {products.map((product) => (
-          <ProductCard key={product.name} product={product} />
-        ))}
+        {products
+          .filter((p) => {
+            if (p.type === "Membership" && hasMembership) return false;
+
+            return true;
+          })
+          .map((product) => (
+            <ProductCard key={product.name} product={product} />
+          ))}
       </Stack>
     </Box>
   );
